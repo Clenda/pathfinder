d20d6829305da8783e6c42827b85f0009eaf5836
Clenda <rabier.nicolas@gmail.com>
2020-11-30 13:26:55 +0100

    fix 2 :
    comment on sql standard
    add charset

--- export/sql/[DELETE]_STARGATE_Pochven_old_gate.sql
+++ export/sql/[DELETE]_STARGATE_Pochven_old_gate.sql
@@ -1,15 +1,20 @@
-/**********************************************************
-* Script Name : [DELETE]_STARGATE_Pochven_old_gate
-* Description : deleted all the standart gate on the Pochven Eve region from the Stargate table
-* Author      : Clenda (Nicolas R.)
-* Controle    : 66 lignes deleted
-*********************************************************/
+---------------------------------------------------------------
+-- Script Name : [DELETE]_STARGATE_Pochven_old_gate
+-- Description : deleted all the standart gate on the Pochven Eve region from the Stargate table
+-- Author      : Clenda (Nicolas R.)
+-- Controle    : 66 lignes deleted
+---------------------------------------------------------------
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 
 DELETE FROM eve_universe.stargate WHERE `name` = 'Stargate (Archee)';
